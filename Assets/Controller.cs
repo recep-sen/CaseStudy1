978526632f21786f9d5f62b24b9cc5fb31678c0c
recep-sen <78692387+recep-sen@users.noreply.github.com>
2021-10-04 19:33:59 +0300

    tried to fix camera rotation

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     Rigidbody rb;
+    public GameObject player_camera;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,8 @@ public class Controller : MonoBehaviour
     {
         Vector3 player_input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         rb.MovePosition(rb.position + player_input * Time.deltaTime*3);
-        
+        Vector3 player_rotation = new Vector3(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"),0 );
+        Quaternion deltaRotation = Quaternion.Euler(player_rotation * Time.fixedDeltaTime);
+        player_camera.transform.rotation *= deltaRotation;
     }
 }
